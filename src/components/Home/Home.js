9a1f60ec0e3cfc22debfd28dfdb9537afac28feb
Caroline <carolnte@gmail.com>
2021-03-28 18:42:58 -0300

    Ainda tentando ajustar página e conteúdo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import '../Home/Home.css'
 class Home extends Component {
 	render() {
 		return (
-			<div>
+			<div className = "divona">
 				<section className="content body">
 					<h1 className="mainTitle">Bons Drinks</h1>
 				</section>				
