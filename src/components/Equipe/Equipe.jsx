bca67b46bdbc5ae591716b63ee16702057859059
Caroline <carolnte@gmail.com>
2021-03-28 16:54:43 -0300

    Adicionando componentes input e página de equipe

---
+++ src/components/Equipe/Equipe.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import style from '../Equipe/equipe.css';
+
+class Team extends Component {
+    render(){
+        return(
+            <div className="divzona">
+            <h1 className="titulo">Sobre Nós</h1>
+            <img className="timeImg fotopessoa1" src="https://www.condemais.com.br/wp/wp-content/uploads/2019/01/C87A0981perfil.jpg"></img>
+            <section className="pessoa1">
+                <h2 className="integrante">Natalia</h2>
+                <p className="textoIntegrante">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel enim consequat lacus consectetur tempus nec sit amet tellus. Nam nec justo lacus. Sed sit.</p>
+            </section>
+            <img className="timeImg fotopessoa2" src="https://s2.glbimg.com/n9NaXMgHka7360FISqoGp4omGYw=/0x0:2362x3544/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2021/A/z/ef2NAbRnuQT2CmHNCLCA/mariana-sena-descreve-sua-personagem-gilda-em-todas-as-mulheres-do-mundo.jpg"></img>
+            <section className="pessoa2">
+                <h2 className="integrante">Gilda</h2>
+                <p className="textoIntegrante">Augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo.</p>
+            </section>
+            <img className="timeImg fotopessoa3" src="https://s2.glbimg.com/OTqXaX_rl0izkov4PQF-QOGH4dQ=/0x0:1883x2824/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2020/P/z/XMRfTCS9ActgiJt359Qg/sophie-charlotte-e-maria-alice-na-serie-todas-as-mulheres-do-mundo.jpg"></img>
+            <section className="pessoa3">
+                <h2 className="integrante">Maria Alice</h2>
+                <p className="textoIntegrante">Integer nec felis sodales, sagittis dolor non, congue libero. Morbi nec fringilla ipsum, ac finibus ligula. Praesent quis iaculis mi, a iaculis odio. Cras quis.</p>
+            </section>
+            <img className="timeImg fotopessoa4" src="https://nedjornal.files.wordpress.com/2020/05/laura-martha-nowill-.jpg?w=600"></img>
+            <section className="pessoa4">
+                <h2 className="integrante">Laura</h2>
+                <p className="textoIntegrante">Nunc varius arcu tortor, sit amet rutrum urna facilisis id. Mauris mattis in massa et condimentum. Proin dui leo, volutpat sed turpis a, malesuada commodo.</p>
+            </section>
+        </div>
+        )
+    }
+}
+
+export default Team;
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useState} from 'react';
+import '../Input/Input.css';
+
+export default function Input(props){
+    return (
+        <div>
+            <label htmlFor={props.htmlFor} className="labelInput">{props.children}</label>
+            <input type={props.tipo} name={props.name} onChange={props.noChange} className="input"></input>
+        </div>
+    )
+}
+
+
+
